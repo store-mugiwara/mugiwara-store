8d5b8cda88a5936e936256b55c38c8722959e2a3
mugiwara <storemugiwara9@gmail.com>
2021-05-03 16:41:01 -0300

    secure asset

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,9 @@
     <title>Loja virtual</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('/css/app.css') }}" rel="stylesheet" />
     @routes
-    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('/js/app.js') }}" defer></script>
   </head>
   <body class="bg-gray-200">
     <div>@inertia</div>
