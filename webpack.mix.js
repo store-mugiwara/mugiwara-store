b89342cfc2b7788d642792f0c67f06054f738ebd
mugiwara <storemugiwara9@gmail.com>
2021-05-07 16:35:31 -0300

    modify

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ require('laravel-mix-tailwind');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require("tailwindcss"),
     ])
     .version()
     .tailwind()
