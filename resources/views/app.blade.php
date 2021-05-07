e87a0c05a0f4eff8d829262274577e5d2f0fc21f
mugiwara <storemugiwara9@gmail.com>
2021-05-07 08:33:34 -0300

    teste css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <title>Loja virtual</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-    <link href="{{ secure_asset('/css/app.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ secure_asset('/css/app.css')}}"  />
     @routes
     <script type="text/javascript" src="{{ secure_asset('/js/app.js')}}" defer></script>
   </head>
